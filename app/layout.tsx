877768b52aad3b8289871fde1163384467d9db12
mehraan09 <aakshh5kaa@gmail.com>
2026-03-05 19:33:20 +0530

    newwww

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,29 @@ const cormorant = Cormorant_Garamond({
 export const metadata: Metadata = {
   title: "A Special Surprise For You 💕",
   description: "A romantic birthday surprise made with love",
+
   openGraph: {
-    title: "Ertiba Akash",
+    title: "Ertiba & Akash",
+    description: "A romantic birthday surprise made with love 💕",
     url: "https://ertibakash.vercel.app",
-    images: [{ url: "/img5.jpg", width: 600, height: 600, alt: "Er" }],
+    siteName: "Birthday Surprise",
+    images: [
+      {
+        url: "/img5.jpg",   // should be 1200x630
+        width: 1200,
+        height: 630,
+        alt: "Ertiba & Akash",
+      },
+    ],
     locale: "en_US",
     type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "A Special Surprise For You 💕",
+    description: "A romantic birthday surprise made with love",
+    images: ["/img5.jpg"],
   },
 };
 
